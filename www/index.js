d912378493623c5c09999342da2e56d6959e5ffd
kenokabe <kenokabe@gmail.com>
2014-12-27 16:39:04 +0900

    commit

--- www/index.js
+++ www/index.js
@@ -3,35 +3,105 @@
 /* jshint sub: true */
 /* global window,document, $,alert,history */
 
+console.log(moment().toString());
+console.log(moment().subtract(3, 'seconds').toString());
+(function() {
 
-//var timelineCapacity = moment.duration(40, 'seconds');
-console.log('----');
-//var ___a = ___(timelineCapacity);
-var ___a = ___();
+    console.log('----');
+    //var ___a = ___(timelineCapacity);
+    var ___a = ___();
 
-//var it = __Natural.getIterator();
+    //var it = __Natural.getIterator();
 
-var interval = setInterval(function()
-{
-  //  it.moveNext();
+    var interval = setInterval(function() {
+      //  it.moveNext();
 
-  // math random 1st simple example
-  ___a.appear(___('NOW').toString());
-}, 1000);
+      // math random 1st simple example
+      var some1 = ___a.appear(___('NOW').toString());
+    }, 1000);
 
-/*
-var ___b = ___a.map(function(x)
-{
-return x * 2;
-});
-*/
+    /*
+    var ___b = ___a.map(function(x)
+    {
+    return x * 2;
+    });
+    */
 
-___a.compute(function()
-{
-  //console.log('a: ' + ___a.value(___('NOW')));
+    var some2 = ___a.compute(function() {
+      //console.log('a: ' + ___a.value(___('NOW')));
+      /*
+          var dom = < h1 > {
+            ___a.value(___('NOW'))
+          } < /h1>;
 
-  var dom =< h1 > {___a.value(___('NOW'))} < /h1>;
+          var c = React
+            .render(dom, document.body);
+        */
+    });
 
-  var c = React
-  .render(dom, document.body );
-});
+    //====================================
+
+    var timelineCapacity = moment.duration(40, 'seconds');
+
+    var ___cursor = ___();
+
+
+
+    var onMouseMove = function(e) {
+
+      var cursor = {
+        x: e.clientX,
+        y: e.clientY
+      };
+
+      ___cursor.appear(cursor);
+    };
+
+    var interval2 = setInterval(function() {
+      //
+      var some1 = ___cursor.appear(___cursor.value(___('NOW')));
+    }, 10);
+
+    /* < h1 > {
+    cursor.x
+  } < /h1>< h1 > {
+  cursor.y
+} < /h1>*/
+
+//console.log(___('NOW'));
+//console.log(moment().subtract(3, 'seconds'));
+
+    var dummy = ___cursor.compute(function() {
+        //var cursor = ___cursor.value(___('NOW'));
+
+    //var cursor = ___cursor.value(moment().subtract(3, 'seconds'));
+    var cursor = ___cursor.value(___('NOW').subtract(1, 'seconds'))
+        var dom = < div >
+      <svg height="600" width="600">
+      <circle cx={cursor.x} cy={cursor.y} r="20" stroke="black" fill="red" />
+      </svg>
+
+      </div > ;
+
+      var c = React
+        .render(dom, document.body);
+
+    });
+
+
+
+
+
+
+
+
+
+  document.addEventListener("mousemove", onMouseMove);
+
+
+
+
+
+
+  //====================================
+})();
