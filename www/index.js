938244b40a344be46907987a62e0cdcc349e1e3a
kenokabe <kenokabe@gmail.com>
2015-01-29 13:03:28 +0900

    commit

--- www/index.js
+++ www/index.js
@@ -1,29 +1,35 @@
 var Component = React.createClass(
+{
+  getInitialState: function()
+  { // 表計算アプリのマス目に相当する
+    return {
+      a: undefined,
+      b: undefined
+    };
+  },
+  handleChange: function(event)
   {
-    getInitialState: function()
-    { // 表計算アプリのマス目に相当する
-      return {
-        a: undefined,
-        b: undefined
-      };
-    },
-    handleChange: function(event)
-    {
-      var a = event.target.value * 1; // aのマス目の値
+    var a = event.target.value * 1; // aのマス目の値
 
-      this.setState(
-        {
-          b: a * 5  // bのマス目の値は、aのマス目の値の5倍と等しい
-        });
-      },
-      render: function()
-      { // aのマス目の値を監視すると同時に
-        return <div>
-        <input type="text" onChange={this.handleChange} />
-        <input type="text" value={this.state.b} />
-        </div>;
-      }　// bのマス目はリアクティブに再計算され
+    this.setState(
+    {
+      b: a * 5 // bのマス目の値は、aのマス目の値の5倍と等しい
     });
+  },
+  render: function()
+    { // aのマス目の値を監視すると同時に
+      return <div >
+        < input type = "text"
+      onChange = {
+        this.handleChange
+      }
+      /> < input type = "text"
+      value = {
+        this.state.b
+      }
+      /> < /div>;
+    }　 // bのマス目はリアクティブに再計算され
+});
 
 var react1 = React.render( <Component/> , document.body);
-    // リアクティブに自動的にスクリーンに再描画される!!
+// リアクティブに自動的にスクリーンに再描画される!!
