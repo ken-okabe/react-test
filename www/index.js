69ea4eb3dff575193e69930d913c6c45cfc248ab
kenokabe <kenokabe@gmail.com>
2015-01-29 13:10:42 +0900

    commit

--- www/index.js
+++ www/index.js
@@ -10,25 +10,15 @@ var Component = React.createClass(
   handleChange: function(event)
   {
     var a = event.target.value * 1; // aのマス目の値
-
-    this.setState(
-    {
-      b: a * 5 // bのマス目の値は、aのマス目の値の5倍と等しい
-    });
+    this.setState({b: a * 5});      // bのマス目の値は、aのマス目の値の5倍と等しい
   },
   render: function()
     { // aのマス目の値を監視すると同時に
-      return <div >
-        < input type = "text"
-      onChange = {
-        this.handleChange
-      }
-      /> < input type = "text"
-      value = {
-        this.state.b
-      }
-      /> < /div>;
-    }　 // bのマス目はリアクティブに再計算され
+      return <div>
+        <input type = "text" onChange = {this.handleChange} />
+        <input type = "text" value = {this.state.b} />
+        </div>;
+    } // bのマス目はリアクティブに再計算され
 });
 
 var react1 = React.render( <Component/> , document.body);
